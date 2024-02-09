6d7f8a1e38d999b67c97e4d94535462d1766ca50
Logan Handley <CMP_LoHandley@ects.org>
2024-02-09 10:37:16 -0500

    Add project files.

---
+++ DownBad.Designer.vb
@@ -0,0 +1,340 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ALLHAIL
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ALLHAIL))
+        Me.btnPraise = New System.Windows.Forms.Button()
+        Me.PictureBox12 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox11 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox10 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.pic7 = New System.Windows.Forms.PictureBox()
+        Me.pic6 = New System.Windows.Forms.PictureBox()
+        Me.pic8 = New System.Windows.Forms.PictureBox()
+        Me.pic4 = New System.Windows.Forms.PictureBox()
+        Me.pic5 = New System.Windows.Forms.PictureBox()
+        Me.pic2 = New System.Windows.Forms.PictureBox()
+        Me.pic3 = New System.Windows.Forms.PictureBox()
+        Me.pic1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnPraise
+        '
+        Me.btnPraise.Location = New System.Drawing.Point(357, 209)
+        Me.btnPraise.Name = "btnPraise"
+        Me.btnPraise.Size = New System.Drawing.Size(125, 23)
+        Me.btnPraise.TabIndex = 0
+        Me.btnPraise.Text = "witness perfection"
+        Me.btnPraise.UseVisualStyleBackColor = True
+        '
+        'PictureBox12
+        '
+        Me.PictureBox12.Image = Global.DownBad.My.Resources.Resources.IMG_0236_1_
+        Me.PictureBox12.Location = New System.Drawing.Point(646, 309)
+        Me.PictureBox12.Name = "PictureBox12"
+        Me.PictureBox12.Size = New System.Drawing.Size(154, 143)
+        Me.PictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox12.TabIndex = 20
+        Me.PictureBox12.TabStop = False
+        '
+        'PictureBox11
+        '
+        Me.PictureBox11.Image = Global.DownBad.My.Resources.Resources.IMG_0241_1_
+        Me.PictureBox11.Location = New System.Drawing.Point(517, 309)
+        Me.PictureBox11.Name = "PictureBox11"
+        Me.PictureBox11.Size = New System.Drawing.Size(133, 143)
+        Me.PictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox11.TabIndex = 19
+        Me.PictureBox11.TabStop = False
+        '
+        'PictureBox10
+        '
+        Me.PictureBox10.Image = Global.DownBad.My.Resources.Resources.IMG_1618_1_
+        Me.PictureBox10.Location = New System.Drawing.Point(599, 168)
+        Me.PictureBox10.Name = "PictureBox10"
+        Me.PictureBox10.Size = New System.Drawing.Size(201, 143)
+        Me.PictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox10.TabIndex = 18
+        Me.PictureBox10.TabStop = False
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.Image = Global.DownBad.My.Resources.Resources.IMG_0648
+        Me.PictureBox9.Location = New System.Drawing.Point(118, 1)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(125, 143)
+        Me.PictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox9.TabIndex = 17
+        Me.PictureBox9.TabStop = False
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.Image = Global.DownBad.My.Resources.Resources.IMG_1549_1_
+        Me.PictureBox8.Location = New System.Drawing.Point(599, 1)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(201, 170)
+        Me.PictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox8.TabIndex = 16
+        Me.PictureBox8.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = Global.DownBad.My.Resources.Resources.IMG_0117_1_
+        Me.PictureBox6.Location = New System.Drawing.Point(478, 142)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(125, 169)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox6.TabIndex = 14
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = Global.DownBad.My.Resources.Resources.IMG_0505
+        Me.PictureBox5.Location = New System.Drawing.Point(237, 1)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(125, 202)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 13
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.DownBad.My.Resources.Resources.IMG_1543_1_
+        Me.PictureBox4.Location = New System.Drawing.Point(478, 1)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(125, 143)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 12
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.DownBad.My.Resources.Resources.IMG_0257
+        Me.PictureBox3.Location = New System.Drawing.Point(357, 1)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(125, 143)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 11
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.DownBad.My.Resources.Resources.IMG_0648
+        Me.PictureBox2.Location = New System.Drawing.Point(646, 278)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(125, 143)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 10
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.DownBad.My.Resources.Resources.IMG_1689_1_
+        Me.PictureBox1.Location = New System.Drawing.Point(357, 142)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(125, 169)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
+        '
+        'pic7
+        '
+        Me.pic7.Image = Global.DownBad.My.Resources.Resources.IMG_0983
+        Me.pic7.Location = New System.Drawing.Point(118, 309)
+        Me.pic7.Name = "pic7"
+        Me.pic7.Size = New System.Drawing.Size(138, 143)
+        Me.pic7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pic7.TabIndex = 8
+        Me.pic7.TabStop = False
+        '
+        'pic6
+        '
+        Me.pic6.Image = Global.DownBad.My.Resources.Resources.IMG_0267
+        Me.pic6.Location = New System.Drawing.Point(-5, 309)
+        Me.pic6.Name = "pic6"
+        Me.pic6.Size = New System.Drawing.Size(138, 143)
+        Me.pic6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pic6.TabIndex = 7
+        Me.pic6.TabStop = False
+        '
+        'pic8
+        '
+        Me.pic8.Image = Global.DownBad.My.Resources.Resources.IMG_1724_1_
+        Me.pic8.Location = New System.Drawing.Point(396, 309)
+        Me.pic8.Name = "pic8"
+        Me.pic8.Size = New System.Drawing.Size(125, 143)
+        Me.pic8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic8.TabIndex = 6
+        Me.pic8.TabStop = False
+        '
+        'pic4
+        '
+        Me.pic4.Image = Global.DownBad.My.Resources.Resources.IMG_0103
+        Me.pic4.Location = New System.Drawing.Point(-5, 195)
+        Me.pic4.Name = "pic4"
+        Me.pic4.Size = New System.Drawing.Size(125, 119)
+        Me.pic4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pic4.TabIndex = 5
+        Me.pic4.TabStop = False
+        '
+        'pic5
+        '
+        Me.pic5.Image = Global.DownBad.My.Resources.Resources.IMG_0345
+        Me.pic5.Location = New System.Drawing.Point(247, 309)
+        Me.pic5.Name = "pic5"
+        Me.pic5.Size = New System.Drawing.Size(152, 143)
+        Me.pic5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic5.TabIndex = 4
+        Me.pic5.TabStop = False
+        '
+        'pic2
+        '
+        Me.pic2.Image = Global.DownBad.My.Resources.Resources.IMG_0209
+        Me.pic2.Location = New System.Drawing.Point(116, 142)
+        Me.pic2.Name = "pic2"
+        Me.pic2.Size = New System.Drawing.Size(125, 169)
+        Me.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pic2.TabIndex = 3
+        Me.pic2.TabStop = False
+        '
+        'pic3
+        '
+        Me.pic3.Image = Global.DownBad.My.Resources.Resources.IMG_0208
+        Me.pic3.Location = New System.Drawing.Point(-5, 1)
+        Me.pic3.Name = "pic3"
+        Me.pic3.Size = New System.Drawing.Size(125, 202)
+        Me.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pic3.TabIndex = 2
+        Me.pic3.TabStop = False
+        '
+        'pic1
+        '
+        Me.pic1.Image = Global.DownBad.My.Resources.Resources.IMG_0091
+        Me.pic1.Location = New System.Drawing.Point(237, 192)
+        Me.pic1.Name = "pic1"
+        Me.pic1.Size = New System.Drawing.Size(125, 119)
+        Me.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pic1.TabIndex = 1
+        Me.pic1.TabStop = False
+        '
+        'ALLHAIL
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnPraise)
+        Me.Controls.Add(Me.PictureBox12)
+        Me.Controls.Add(Me.PictureBox11)
+        Me.Controls.Add(Me.PictureBox10)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.pic7)
+        Me.Controls.Add(Me.pic6)
+        Me.Controls.Add(Me.pic8)
+        Me.Controls.Add(Me.pic4)
+        Me.Controls.Add(Me.pic5)
+        Me.Controls.Add(Me.pic2)
+        Me.Controls.Add(Me.pic3)
+        Me.Controls.Add(Me.pic1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "ALLHAIL"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "perfection"
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnPraise As Button
+    Friend WithEvents pic1 As PictureBox
+    Friend WithEvents pic3 As PictureBox
+    Friend WithEvents pic2 As PictureBox
+    Friend WithEvents pic5 As PictureBox
+    Friend WithEvents pic4 As PictureBox
+    Friend WithEvents pic8 As PictureBox
+    Friend WithEvents pic6 As PictureBox
+    Friend WithEvents pic7 As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents PictureBox9 As PictureBox
+    Friend WithEvents PictureBox10 As PictureBox
+    Friend WithEvents PictureBox11 As PictureBox
+    Friend WithEvents PictureBox12 As PictureBox
+End Class
